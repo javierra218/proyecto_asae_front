192c6b94a69b23d59de3f41fd64ac534850abc45
Javier Rojas <javierra@unicauca.edu.co>
2024-11-18 00:35:36 -0500

    feat: agregar nuevas dependencias y mejorar la gestión de clientes

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -2,15 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { CommonModule } from '@angular/common';
 import { ClienteService } from '../cliente.service';
-
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'; 
 
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css'],
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, RouterModule],
 })
 export class ClientesComponent implements OnInit {
   clientes: Cliente[] = [];
@@ -19,6 +19,7 @@ export class ClientesComponent implements OnInit {
 
   ngOnInit(): void {
     this.clienteService.getClientes().subscribe((clientes) => {
+      console.log('Listado de clientes:', clientes);
       this.clientes = clientes;
     });
   }
--- src/app/clientes/crearCliente/form/form.component.ts
+++ src/app/clientes/crearCliente/form/form.component.ts
@@ -2,30 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Cliente } from '../../cliente';
 import { ClienteService } from '../../../cliente.service';
 import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms'; 
+import {SweetAlert2Module} from '@sweetalert2/ngx-sweetalert2';
+import Swal from 'sweetalert2';
+
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
   standalone: true,
+  imports: [FormsModule,SweetAlert2Module],
 })
 export class FormComponent implements OnInit {
-
   public cliente: Cliente = new Cliente();
-  public titulo: string = "Crear Cliente";
+  public titulo: string = 'Crear Cliente';
 
-  constructor(private clienteService: ClienteService, private router: Router) { }
+  constructor(private clienteService: ClienteService, private router: Router) {}
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   public crearCliente(): void {
     console.log('creando cliente');
-    this.clienteService.create(this.cliente).subscribe(
-      response => {
-        console.log('cliente creado exitosamente');
-        console.log(this.cliente);
-        this.router.navigate(['/clientes/listarClientes']);
-      }
-    );
+    this.clienteService.create(this.cliente).subscribe((response) => {
+      console.log('cliente creado exitosamente');
+      console.log(this.cliente);
+      this.router.navigate(['/clientes/listarClientes']);
+      Swal.fire('Nuevo Cliente', `Cliente ${this.cliente.nombre} creado con éxito`, 'success');
+    });
   }
 }
